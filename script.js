0f99bb2ec4e2cc400120ce1fdcd786d7c0a13e66
chimpbros <kevinnue00@gmail.com>
2022-12-11 13:01:06 +0700

    Create playRound function on script.js

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
+// create an array called "choices" consists of rock, paper, and scissors
+let choices = Array("rock", "paper", "scissors");
+
 // 1. get the computer to return random choice between rock, paper , or scissor
 //    - create a function "getComputerChoice" that randomly return either rock, paper, or scissors
 function getComputerChoice() {
-    // create an array called "choices" consists of rock, paper, and scissors
-    let choices = Array("rock", "paper", "scissors");
     // create a variable called "computerChoice" to hold the computer choice
     let computerChoice;
     // randomly select an item from the array
@@ -11,3 +12,32 @@ function getComputerChoice() {
     // return "computerChoice"
     return computerChoice;
 }
+// 2. play a single round of rock paper scissors
+//    - create a function that plays a single round of RPS that takes two parameters - playerSelection and computerSelection
+function playRound(playerSelection, computerSelection){
+    // if(define condition for player win)
+    if ((playerSelection === "rock" &&  computerSelection === "scissors") || (playerSelection === "paper" && computerSelection === "rock") || (playerSelection === "scissors" && computerSelection === "paper")){
+        return "You Win, " + playerSelection + " beats " + computerSelection;
+    }
+    // else if(define condition for computer win)
+    else if ((computerSelection === "rock" && playerSelection === "scissors") || (computerSelection === "paper" && playerSelection === "rock") || (computerSelection === "scissors" && playerSelection === "paper")){
+        return "You Lose, " + computerSelection + " beats " + playerSelection
+    }
+    // else
+    else{
+        // return tie
+        return "Tie"
+    }
+}
+// 3. play a game consists of 5 round
+//    - create a function game()
+function game(){
+    //- call playround() 5 times using for loop
+    for(let i = 0; i < 5; i++){
+        playerSelection = prompt("choose your weapon")
+    }
+
+}
+//    - keep track of the score inside variables
+//    - reports the winner
+
