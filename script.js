69b769b45e1d57cc36bf7ad1e93533a6f8e79e32
chimpbros <kevinnue00@gmail.com>
2022-12-16 14:46:27 +0700

    Add text content feature on announce div (in progress)

--- script.js
+++ script.js
@@ -33,44 +33,26 @@ function playRound(playerSelection, computerSelection){
 }
 // 3. play a game consists of 5 round
 //    - create a function game()
-function game(round){
-    //- call playround() 5 times using for loop
+function game(result){
     // keep track of the score inside variables
     let playerScore = 0;
     let computerScore = 0;
-    for(let i = 0; i < round; i++){
-        // prompt user for choice
-        var playerSelection = prompt("choose your weapon");
-        // convert choice to lower case
-        var selectionLower = playerSelection.toLowerCase();
-        // ensure user input are either rock paper or scissors
-        while(true){
-            if(choices.includes(selectionLower)){
-                break
-            }
-            else{console.log("please enter a valid weapon");}
-            playerSelection = prompt("choose your weapon");
-            selectionLower = playerSelection.toLowerCase();
-        }
-        // play 5 round game
-        let computerSelection = getComputerChoice();
-        let result = playRound(selectionLower, computerSelection);
 
-        // if player win increment playerscore by 1
-        if(result === 1){
-            playerScore++;
-            console.log("You Win! " + selectionLower + " beats " + computerSelection);
-        }
-        // if player lose increment computer score by 1
-        else if(result === 2){
-            computerScore++;
-            console.log("You Lose! " + computerSelection + " beats " + selectionLower);
-        }
-        else{console.log("Tie");}
-        // print current score
-        console.log("Player Score: " + playerScore.toString());
-        console.log("Computer Score: " + computerScore.toString());
+    // if player win increment playerscore by 1
+    if(result === 1){
+        playerScore++;
+        announce.textContent = `You Win!`;
     }
+    // if player lose increment computer score by 1
+    else if(result === 2){
+        computerScore++;
+        announce.textContent = `You Lose!`
+    }
+    else{announce.textContent = `Tie`;}
+    // print current score
+    console.log("Player Score: " + playerScore.toString());
+    console.log("Computer Score: " + computerScore.toString());
+    
     //    - reports the winner
     if(playerScore > computerScore){
         console.log("Congratulations, You Win!");
@@ -78,26 +60,27 @@ function game(round){
     else if(computerScore > playerScore){
         console.log("You Lose!");
     }
-    else{console.log("Tie!")}
+    else{console.log("Tie!");}
 }
 
 const rock = document.querySelector('#rock');
 const paper = document.querySelector('#paper');
 const scissors = document.querySelector('#scissors');
-let computerChoice = getComputerChoice();
+const announce = document.querySelector('#announce');
+let computerChoice;
 
 rock.addEventListener('click', () => {
     computerChoice = getComputerChoice();
-    let winner = playRound('rock', computerChoice);
-    console.log(winner);
+    let result = playRound('rock', computerChoice);
+    game(result);
 });
 paper.addEventListener('click', () => {
     computerChoice = getComputerChoice();
-    let winner = playRound('paper', computerChoice);
-    console.log(winner);
+    let result = playRound('paper', computerChoice);
+    console.log(result);
 });
 scissors.addEventListener('click', () => {
     computerChoice = getComputerChoice();
-    let winner = playRound('scissors', computerChoice);
-    console.log(winner);
+    let result = playRound('scissors', computerChoice);
+    console.log(result);
 });
