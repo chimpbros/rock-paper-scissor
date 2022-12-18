a69c02ff6417e64b11e8fe9d1404ea029808c9a2
chimpbros <kevinnue00@gmail.com>
2022-12-18 09:10:22 +0700

    Add selection display and result display

--- script.js
+++ script.js
@@ -15,62 +15,44 @@ function getComputerChoice() {
 // 2. play a single round of rock paper scissors
 //    - create a function that plays a single round of RPS that takes two parameters - playerSelection and computerSelection
 function playRound(playerSelection, computerSelection){
+    playerInfo.textContent = `player: ${playerSelection}`;
+    computerInfo.textContent = `computer: ${computerSelection}`;
     // if(define condition for player win)
     if ((playerSelection === "rock" &&  computerSelection === "scissors") || (playerSelection === "paper" && computerSelection === "rock") || (playerSelection === "scissors" && computerSelection === "paper")){
+        playerScore++;
+        gameInfo.textContent = `You Win!, ${playerSelection} beats ${computerSelection}`
         // return 1 if player wins
         return 1;
     }
     // else if(define condition for computer win)
     else if ((computerSelection === "rock" && playerSelection === "scissors") || (computerSelection === "paper" && playerSelection === "rock") || (computerSelection === "scissors" && playerSelection === "paper")){
         // return 2 if computer wins
+        computerScore++;
+        gameInfo.textContent = `You Lose!, ${computerSelection} beats ${playerSelection}`
         return 2;
     }
     // else
     else{
         // return 0 if tie
+        gameInfo.textContent = 'Tie'
         return 0;
     }
 }
-// 3. play a game consists of 5 round
-//    - create a function game()
-function game(){
-    //- call playround() 5 times using for loop
-    // keep track of the score inside variables
-    let playerScore = 0;
-    let computerScore = 0;
-    for(let i = 0; i < 5; i++){
-        // prompt user for choice
-        var playerSelection = prompt("choose your weapon");
-        // convert choice to lower case
-        var selectionLower = playerSelection.toLowerCase();
-        // ensure user input are either rock paper or scissors
-        while(true){
-            if(choices.includes(selectionLower)){
-                break
-            }
-            else{console.log("please enter a valid weapon");}
-            playerSelection = prompt("choose your weapon");
-            selectionLower = playerSelection.toLowerCase();
-        }
-        // play 5 round game
-        let computerSelection = getComputerChoice();
-        let result = playRound(selectionLower, computerSelection);
-
-        // if player win increment playerscore by 1
-        if(result === 1){
-            playerScore++;
-            console.log("You Win! " + selectionLower + " beats " + computerSelection);
-        }
-        // if player lose increment computer score by 1
-        else if(result === 2){
-            computerScore++;
-            console.log("You Lose! " + computerSelection + " beats " + selectionLower);
-        }
-        else{console.log("Tie");}
-        // print current score
-        console.log("Player Score: " + playerScore.toString());
-        console.log("Computer Score: " + computerScore.toString());
+function round(result){
+    // if player win increment playerscore by 1
+    if(result === 1){
+        playerScore++;
+        console.log("You Win! " + selectionLower + " beats " + computerSelection);
     }
+    // if player lose increment computer score by 1
+    else if(result === 2){
+        computerScore++;
+        console.log("You Lose! " + computerSelection + " beats " + selectionLower);
+    }
+    else{console.log("Tie");}
+    // print current score
+    console.log("Player Score: " + playerScore.toString());
+    console.log("Computer Score: " + computerScore.toString());
     //    - reports the winner
     if(playerScore > computerScore){
         console.log("Congratulations, You Win!");
@@ -81,5 +63,30 @@ function game(){
     else{console.log("Tie!")}
 }
 
-game()
+// function to execute when button clicked
+function playEvent(playerSelection){
+    computerSelect = getComputerChoice();
+    let result = playRound(playerSelection, computerSelect);
+    return result;
+}
 
+
+const rock = document.querySelector('#rock');
+const paper = document.querySelector('#paper');
+const scissors = document.querySelector('#scissors');
+const gameInfo = document.querySelector('#game-info');
+const playerInfo = document.querySelector('#player-select');
+const computerInfo = document.querySelector('#computer-select');
+let computerSelect;
+let playerScore = 0;
+let computerScore = 0;
+
+rock.addEventListener('click', () => {
+    playEvent('rock');
+});
+paper.addEventListener('click', () => {
+    playEvent('paper');
+});
+scissors.addEventListener('click', () => {
+    playEvent('scissors');
+});
